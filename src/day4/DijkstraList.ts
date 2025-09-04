6b48f688691ee56b1e1f781c14834ec2e9e6dbbc
Graham <grahamtallen94@gmail.com>
2025-09-04 16:24:13 -0700

    completed inefficient dijkstra

--- src/day4/DijkstraList.ts
+++ src/day4/DijkstraList.ts
@@ -21,12 +21,34 @@ export default function dijkstra_list(
     arr: WeightedAdjacencyList,
 ): number[] {
     const seen: boolean[] = new Array(arr.length).fill(false);
+    const prev: number[] = new Array(arr.length).fill(-1);
     const distances: number[] = new Array(arr.length).fill(Infinity);
 
     distances[source] = 0;
 
     while (hasUnvisited(seen, distances)) {
         const lowest = getLowest(seen, distances);
+        seen[lowest] = true;
+        const adjacencies = arr[lowest];
+        for (let i = 0; i < adjacencies.length; i++) {
+            const edge = adjacencies[i];
+            if (seen[edge.to]) continue;
+            const dist = distances[lowest] + edge.weight;
+            if (dist < distances[edge.to]) {
+                distances[edge.to] = dist;
+                prev[edge.to] = lowest;
+            }
+        }
+    }
+    const out: number[] = [];
+    let current = sink;
+    while (prev[current] !== -1) {
+        out.push(current);
+        current = prev[current];
+    }
+    if (out.length) {
+        out.push(source);
+        return out.reverse();
     }
     return [];
 }
