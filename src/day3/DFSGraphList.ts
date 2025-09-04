4c7e32e7c8e8758351522ef97e814e1332d3d5d5
Graham <grahamtallen94@gmail.com>
2025-09-03 23:03:54 -0700

    starting on graph list

---
+++ src/day3/DFSGraphList.ts
@@ -0,0 +1,12 @@
+const walk = (
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: number[],
+    path: number[],
+): number[] => {};
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {}
