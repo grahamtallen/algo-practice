1a1c7514f77d5d7aef9052ff6c6f604d0634d06f
Graham <grahamtallen94@gmail.com>
2025-09-04 00:43:41 -0700

    utility fns

--- src/day4/DijkstraList.ts
+++ src/day4/DijkstraList.ts
@@ -1,3 +1,32 @@
-export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
+const hasUnvisited = (seen: boolean[], distances: number[]): boolean => {
+    return seen.some((s, i) => !s && distances[i] < Infinity);
+};
+const getLowest = (seen: boolean[], distances: number[]): number => {
+    let lowestIndex = -1;
+    let lowestDistance = Infinity;
+    for (let i = 0; i < seen.length; i++) {
+        if (seen[i]) {
+            continue;
+        }
+        if (distances[i] < lowestDistance) {
+            lowestDistance = distances[i];
+            lowestIndex = i;
+        }
+    }
+    return lowestIndex;
+};
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList,
+): number[] {
+    const seen: boolean[] = new Array(arr.length).fill(false);
+    const distances: number[] = new Array(arr.length).fill(Infinity);
 
+    distances[source] = 0;
+
+    while (hasUnvisited(seen, distances)) {
+        const lowest = getLowest(seen, distances);
+    }
+    return [];
 }
